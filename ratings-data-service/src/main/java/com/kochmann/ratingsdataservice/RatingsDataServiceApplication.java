af49d451727db42c539d8f45730ecf155d5ec676
Cristina Kochmann <cristinak.iftm@gmail.com>
2021-11-08 20:22:16 -0300

    Commit inicial Projeto Microservices Filmes

---
+++ ratings-data-service/src/main/java/com/kochmann/ratingsdataservice/RatingsDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kochmann.ratingsdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsDataServiceApplication.class, args);
+	}
+
+}
