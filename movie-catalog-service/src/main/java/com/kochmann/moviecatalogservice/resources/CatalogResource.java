71eab610505bc3a1748e4f6fdd802199a63225ce
Cristina Kochmann <cristinak.iftm@gmail.com>
2021-11-21 19:09:15 -0300

    Usando Bean para criar uma instância de RestTemplate para MovieCatalog

--- movie-catalog-service/src/main/java/com/kochmann/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/com/kochmann/moviecatalogservice/resources/CatalogResource.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,29 +18,42 @@ import com.kochmann.moviecatalogservice.models.Rating;
 @RequestMapping("/catalog")
 public class CatalogResource {
 
+	/*
+	 * Bean informa que tem algum dado disponível e o Autowired informa que precisa
+	 * desse dado.
+	 * 
+	 * Autowired informa ao Spring que em algum lugar (MovieatalogServideApp) existe
+	 * um BEAN desse RestTemplate e o inject precisa desse dados e é injetado aqui
+	 */
+	@Autowired
+	private RestTemplate restTemplate = new RestTemplate();
+
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-				
-		RestTemplate restTemplate = new RestTemplate();
-		
+
 		// 1) Obter todos os movies IDs
-		
-		List<Rating> ratingList = Arrays.asList(new Rating("12", 5), new Rating("15", 2));
 
-		return ratingList.stream().map(rating -> {
-		// 2) Para cada movie Id, chamar movie info service e get details
-		Movie movie = restTemplate.getForObject("http://localhost:8082/movies/ " + rating.getMovieId(), Movie.class);
+		List<Rating> ratings = Arrays.asList(new Rating("12", 5), new Rating("15", 2));
 
-		return new CatalogItem(movie.getName(), "filmao", rating.getRating());
+	
+		return ratings.stream().map(rating -> {
+			
+			// 2) Para cada movie Id, chamar movie info service e get details
+			
+			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/ " + rating.getMovieId(),
+					Movie.class);
+
+			return new CatalogItem(movie.getName(), "filmao", rating.getRating());
 		})
-				
-		// 3) Colocar tudo junto
+
+				// 3) Colocar tudo junto
 				.collect(Collectors.toList());
 
-		// retorno inicial, para teste
-
-		// return Collections.singletonList(
-		// new CatalogItem("Transformers","Filme de Robô", 4));
-
+		/*
+		 * retorno inicial, para teste
+		 * 
+		 * return Collections.singletonList( // new
+		 * CatalogItem("Transformers","Filme de Robô", 4));
+		 */
 	}
 }
