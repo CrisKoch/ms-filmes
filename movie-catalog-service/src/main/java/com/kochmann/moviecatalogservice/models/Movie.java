fce10f5b77c8dee5df3c76df1e663b31a282376b
Cristina Kochmann <cristinak.iftm@gmail.com>
2021-11-29 18:01:40 -0300

    Adicionado Discovery Server Eureka

--- movie-catalog-service/src/main/java/com/kochmann/moviecatalogservice/models/Movie.java
+++ movie-catalog-service/src/main/java/com/kochmann/moviecatalogservice/models/Movie.java
@@ -1,30 +1,37 @@
 package com.kochmann.moviecatalogservice.models;
 
 public class Movie {
-	
+
 	private String movieId;
 	private String name;
 
+	// Aqui indica como esta classe poderá ser acessada
 	public Movie(String movieId, String name) {
 		super();
 		this.movieId = movieId;
 		this.name = name;
 	}
+
+	// obj vazio, acessado depois populado
 	public Movie() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public String getMovieId() {
 		return movieId;
 	}
+
 	public void setMovieId(String movieId) {
 		this.movieId = movieId;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 }
--- ratings-data-service/src/main/java/com/kochmann/ratingsdataservice/resources/RatingsResource.java
+++ ratings-data-service/src/main/java/com/kochmann/ratingsdataservice/resources/RatingsResource.java
@@ -1,17 +1,30 @@
 package com.kochmann.ratingsdataservice.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kochmann.ratingsdataservice.models.Rating;
+import com.kochmann.ratingsdataservice.models.UserRating;
 
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingsResource {
-	
+
 	@RequestMapping("/{movieId}")
 	public Rating getRating(@PathVariable("movieId") String movieId) {
 		return new Rating(movieId, 4);
+	}
+
+	@RequestMapping("users/{userId}")
+	public List<Rating> getUserRating(@PathVariable("userId") String userId) {
+		List<Rating> ratings = Arrays.asList(new Rating("1234", 15), new Rating("1541", 20));
+		UserRating userRating = new UserRating();
+		userRating.setUserRating(ratings);
+
+		return ratings;
 	}
 }
